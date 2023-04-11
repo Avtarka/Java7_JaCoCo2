d38a76e77828c6b5a864bbbda0e0febaadcc86ff
Pavel Kryuchkov <mr.p@bk.ru>
2023-04-11 23:51:15 +0400

    add JaCoCo

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -16,4 +16,28 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxPermutation() {
+        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+
+        long[] incomesInBillions = {3, 5, 8, 4, 5, 12, 8, 6, 11, 12, 11};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxNegativeValues() {
+        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+
+        long[] incomesInBillions = {-5, -12, -8, -4, -5, -1, -8, -6, -11, -10, -3};
+        long expected = -1;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
